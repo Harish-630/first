34fb77bc16daacd75f3c6f396823a99b5766885c
Harish-630 <harishkommu01@gmail.com>
2025-05-24 22:15:17 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  region = "ap-south-1"  
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0af9569868786b23a" 
+  instance_type = "t2.micro"
+  key_name      = "25/05/2025" 
+
+  tags = {
+    Name = "MySimpleInstance"
+  }
+}
+
